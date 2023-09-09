a22821a9519f723f340fef29dbd72e7d5616f1f3
EnergyNick <nsikachin@gmail.com>
2023-09-09 15:15:49 +0300

    Added extensions for fluent Result creations

---
+++ src/SimpleResult/Extensions/ObjectExtensions.cs
@@ -0,0 +1,14 @@
+﻿using SimpleResult.Core;
+
+namespace SimpleResult.Extensions;
+
+public static class ObjectExtensions
+{
+    public static Result<TValue> MakeResult<TValue>(this TValue value) => Result.Ok(value);
+
+    public static Result<TValue> MakeFailedResult<TValue>(this IError error) =>
+        Result.Fail<TValue>(error);
+
+    public static Result<TValue> MakeFailedResult<TValue>(this Exception exception)=>
+        Result.Fail<TValue>(exception);
+}
