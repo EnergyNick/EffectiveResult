446d8db6bbd92f2cab78a517df9bc1093f43ce5f
EnergyNick <nsikachin@gmail.com>
2023-09-09 14:22:15 +0300

    Added base exception hierarchy

---
+++ src/SimpleResult.Core/Exceptions/OperationOnFailedResultException.cs
@@ -0,0 +1,38 @@
+﻿#pragma warning disable CS1573
+
+namespace SimpleResult.Exceptions;
+
+/// <summary>
+/// Thrown when operation invoked on unsuccessful result
+/// </summary>
+public class OperationOnFailedResultException : ResultException
+{
+    private const string ExceptionMessage = "Result is in failed status, can't do operation";
+
+    /// <inheritdoc />
+    /// <param name="operationName">Name of operation, that threw the exception</param>
+    public OperationOnFailedResultException(string operationName) : base(FailedResultMessage(operationName))
+    { }
+
+    /// <inheritdoc />
+    /// <param name="operationName">Name of operation, that threw the exception</param>
+    public OperationOnFailedResultException(string operationName, Exception innerException)
+        : base(FailedResultMessage(operationName), innerException)
+    { }
+
+    /// <inheritdoc />
+    /// <param name="operationName">Name of operation, that threw the exception</param>
+    public OperationOnFailedResultException(string message, string operationName)
+        : base(FailedResultMessage(operationName) + message)
+    { }
+
+    /// <inheritdoc />
+    public OperationOnFailedResultException(string message, string operationName, Exception innerException)
+        : base(FailedResultMessage(operationName) + message, innerException)
+    { }
+
+    private static string FailedResultMessage(string? operationName) =>
+        operationName != null
+            ? $"{ExceptionMessage}: {operationName}."
+            : ExceptionMessage;
+}
---
+++ src/SimpleResult.Core/Exceptions/ResultException.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleResult.Exceptions;
+
+/// <summary>
+/// Base exception for all incorrect operations with result
+/// </summary>
+public class ResultException : Exception
+{
+    /// <inheritdoc />
+    public ResultException(string message) : base(message)
+    { }
+
+    /// <inheritdoc />
+    public ResultException(string message, Exception innerException) : base(message, innerException)
+    { }
+}
