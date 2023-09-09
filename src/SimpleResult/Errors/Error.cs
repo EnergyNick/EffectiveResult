9970067361bcba512186513f72c03adb1fdad27b
EnergyNick <nsikachin@gmail.com>
2023-09-09 15:07:19 +0300

    Added Errors realization

---
+++ src/SimpleResult/Errors/Error.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Immutable;
+using SimpleResult.Core;
+
+namespace SimpleResult;
+
+public record Error(string Message) : IError
+{
+    private readonly ImmutableArray<IError> _causedErrors = ImmutableArray<IError>.Empty;
+
+    public IReadOnlyCollection<IError> CausedErrors => _causedErrors;
+
+    public Error(string message, IError causedBy) : this(message) =>
+        _causedErrors = ImmutableArray.Create(causedBy);
+
+    public Error(string message, params IError[] causedBy) : this(message) =>
+        _causedErrors = ImmutableArray.Create(causedBy);
+
+    public Error(string message, IEnumerable<IError> causedBy) : this(message) =>
+        _causedErrors = causedBy.ToImmutableArray();
+}
