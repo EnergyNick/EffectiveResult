72b5d2ea7028fded0c2d6b452b23cfa6f9ac6f79
EnergyNick <nsikachin@gmail.com>
2023-09-09 15:07:57 +0300

    Added static settings for Results

---
+++ src/SimpleResult/Settings/ResultParameters.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleResult.Settings;
+
+/// <summary>
+/// Represent parameters of global settings of <see cref="Result"/>
+/// </summary>
+public class ResultParameters
+{
+    /// <summary>
+    /// Provide converting of exception in try/catch blocks
+    /// </summary>
+    public Func<Exception, Error> TryCatchHandler { get; init; }
+}
