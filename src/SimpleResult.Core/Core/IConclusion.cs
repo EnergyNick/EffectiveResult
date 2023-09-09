e284da25d5a5f0092d1548d665c612cdccb54121
EnergyNick <nsikachin@gmail.com>
2023-09-09 14:02:04 +0300

    Added core library interfaces

---
+++ src/SimpleResult.Core/Core/IConclusion.cs
@@ -0,0 +1,23 @@
+namespace SimpleResult.Core;
+
+/// <summary>
+/// The base type of results, represent only final state of operation.
+/// Used for core abstraction of operations results.
+/// </summary>
+public interface IConclusion
+{
+    /// <summary>
+    /// Is true, if contains no errors.
+    /// </summary>
+    bool IsSuccess { get; }
+
+    /// <summary>
+    /// Is true, if contains any errors.
+    /// </summary>
+    bool IsFailed { get; }
+
+    /// <summary>
+    /// Get all errors.
+    /// </summary>
+    IEnumerable<IError> Errors { get; }
+}
---
+++ src/SimpleResult.Core/Core/IError.cs
@@ -0,0 +1,18 @@
+namespace SimpleResult.Core;
+
+/// <summary>
+/// Represents the base type of all error causes.
+/// </summary>
+public interface IError
+{
+    /// <summary>
+    /// Gets a message that describes the current error.
+    /// </summary>
+    /// <returns>The error message that explains the reason, or an empty string ("").</returns>
+    string Message { get; }
+
+    /// <summary>
+    /// Errors causing this error.
+    /// </summary>
+    IReadOnlyCollection<IError> CausedErrors { get; }
+}
