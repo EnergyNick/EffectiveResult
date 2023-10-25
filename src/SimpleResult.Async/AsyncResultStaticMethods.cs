3e00ed18449576026e7af31e90fe51f094785e5d
EnergyNick <nsikachin@gmail.com>
2023-10-25 12:29:49 +0300

    Added static factories for AsyncResult

---
+++ src/SimpleResult.Async/AsyncResultStaticMethods.cs
@@ -0,0 +1,32 @@
+﻿namespace SimpleResult.Async;
+
+public partial class AsyncResult
+{
+    /// <summary>
+    /// Gets a successfully completed result.
+    /// </summary>
+    public static AsyncResult CompletedResult { get; } = new(Result.Ok());
+
+    /// <summary>
+    /// Create competed async result with provided result.
+    /// </summary>
+    /// <param name="result">Result for state of completed result</param>
+    /// <returns>Completed result with state on input result</returns>
+    public static AsyncResult FromResult(Result result) => new(result);
+
+    /// <summary>
+    /// Create competed async result with provided result.
+    /// </summary>
+    /// <param name="result">Result for state of completed result</param>
+    /// <typeparam name="TValue">Type of results value</typeparam>
+    /// <returns>Completed result with state on input result</returns>
+    public static AsyncResult<TValue> FromResult<TValue>(Result<TValue> result) => new(result);
+
+    /// <summary>
+    /// Create competed async result from provided value.
+    /// </summary>
+    /// <param name="value">Value of new success result</param>
+    /// <typeparam name="TValue">Type of value</typeparam>
+    /// <returns>Completed result with successful state with input value</returns>
+    public static AsyncResult<TValue> FromValue<TValue>(TValue value) => new(value);
+}
