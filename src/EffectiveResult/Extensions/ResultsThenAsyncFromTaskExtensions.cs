dd303bafbec91dc6a7c70bce8c56475daf40583f
EnergyNick <nsikachin@gmail.com>
2025-10-12 02:25:49 +0500

    Add async extension methods

---
+++ src/EffectiveResult/Extensions/ResultsThenAsyncFromTaskExtensions.cs
@@ -0,0 +1,130 @@
+﻿namespace EffectiveResult.Extensions;
+
+public static partial class ResultsThenAsyncExtensions
+{
+    /// <summary>
+    /// Provide chaining method for async action on success result
+    /// </summary>
+    /// <param name="input">Source result</param>
+    /// <param name="continuation">Async action for invoke on success</param>
+    /// <returns>Result of <paramref name="continuation"/> or errors from <paramref name="input"/></returns>
+    public static async Task<Result> ThenAsync(this Task<Result> input, Func<Task> continuation)
+    {
+        var inputResult = await input;
+        return inputResult.IsSuccess
+            ? await Result.TryAsync(continuation)
+            : inputResult;
+    }
+
+    /// <summary>
+    /// Provide chaining method for action on success result
+    /// </summary>
+    /// <param name="input">Source result</param>
+    /// <param name="continuation">Async action for invoke on success</param>
+    /// <typeparam name="TValue">Type of result value on success</typeparam>
+    /// <returns>Result of <paramref name="continuation"/> or errors from <paramref name="input"/></returns>
+    public static async Task<Result> ThenAsync<TValue>(this Task<Result<TValue>> input, Func<TValue, Task> continuation)
+    {
+        var inputResult = await input;
+        return inputResult.IsSuccess
+            ? await Result.TryAsync(() => continuation(inputResult.Value))
+            : inputResult.ToResult();
+    }
+
+    /// <summary>
+    /// Provide chaining method for next async operation on success result.
+    /// </summary>
+    /// <param name="input">Source result</param>
+    /// <param name="continuation">Async function for invoke on success</param>
+    /// <typeparam name="TOutput">Type of "<paramref name="continuation"/>" result</typeparam>
+    /// <returns>Result of <paramref name="continuation"/> or errors from <paramref name="input"/></returns>
+    public static async Task<Result<TOutput>> ThenAsync<TOutput>(this Task<Result> input, Func<Task<TOutput>> continuation)
+    {
+        var inputResult = await input;
+        return inputResult.IsSuccess
+            ? await Result.TryAsync(continuation)
+            : inputResult.ToResult<TOutput>();
+    }
+
+    /// <summary>
+    /// Provide chaining method for next async operation on success result.
+    /// </summary>
+    /// <param name="input">Source result</param>
+    /// <param name="continuation">Async function for invoke on success</param>
+    /// <returns>Result of <paramref name="continuation"/> or errors from <paramref name="input"/></returns>
+    public static async Task<Result> ThenAsync(this Task<Result> input, Func<Task<Result>> continuation)
+    {
+        var inputResult = await input;
+        return inputResult.IsSuccess
+            ? await continuation()
+            : inputResult;
+    }
+
+    /// <summary>
+    /// Provide chaining method for next async operation on success result.
+    /// </summary>
+    /// <param name="input">Source result</param>
+    /// <param name="continuation">Async function for invoke on success</param>
+    /// <typeparam name="TOutput">Type of "<paramref name="continuation"/>" result</typeparam>
+    /// <returns>Result of <paramref name="continuation"/> or errors from <paramref name="input"/></returns>
+    public static async Task<Result<TOutput>> ThenAsync<TOutput>(this Task<Result> input, Func<Task<Result<TOutput>>> continuation)
+    {
+        var inputResult = await input;
+        return inputResult.IsSuccess
+            ? await continuation()
+            : inputResult.ToResult<TOutput>();
+    }
+
+    /// <summary>
+    /// Provide chaining method for next async operation on success result.
+    /// </summary>
+    /// <param name="input">Source result</param>
+    /// <param name="continuation">Async function for invoke on success</param>
+    /// <typeparam name="TInput">Type <paramref name="input"/> result</typeparam>
+    /// <typeparam name="TOutput">Type of "<paramref name="continuation"/>" result</typeparam>
+    /// <returns>Result of <paramref name="continuation"/> or errors from <paramref name="input"/></returns>
+    public static async Task<Result<TOutput>> ThenAsync<TInput, TOutput>(
+        this Task<Result<TInput>> input,
+        Func<TInput, Task<TOutput>> continuation)
+    {
+        var inputResult = await input;
+        return inputResult.IsSuccess
+            ? await Result.TryAsync(() => continuation(inputResult.Value))
+            : inputResult.ToResult<TOutput>();
+    }
+
+    /// <summary>
+    /// Provide chaining method for next async operation on success result.
+    /// </summary>
+    /// <param name="input">Source result</param>
+    /// <param name="continuation">Async function for invoke on success</param>
+    /// <typeparam name="TInput">Type <paramref name="input"/> result</typeparam>
+    /// <returns>Result of <paramref name="continuation"/> or errors from <paramref name="input"/></returns>
+    public static async Task<Result> ThenAsync<TInput>(
+        this Task<Result<TInput>> input,
+        Func<TInput, Task<Result>> continuation)
+    {
+        var inputResult = await input;
+        return inputResult.IsSuccess
+            ? await continuation(inputResult.Value)
+            : inputResult.ToResult();
+    }
+
+    /// <summary>
+    /// Provide chaining method for next async operation on success result.
+    /// </summary>
+    /// <param name="input">Source result</param>
+    /// <param name="continuation">Async function for invoke on success</param>
+    /// <typeparam name="TInput">Type <paramref name="input"/> result</typeparam>
+    /// <typeparam name="TOutput">Type of "<paramref name="continuation"/>" result</typeparam>
+    /// <returns>Result of <paramref name="continuation"/> or errors from <paramref name="input"/></returns>
+    public static async Task<Result<TOutput>> ThenAsync<TInput, TOutput>(
+        this Task<Result<TInput>> input,
+        Func<TInput, Task<Result<TOutput>>> continuation)
+    {
+        var inputResult = await input;
+        return inputResult.IsSuccess
+            ? await continuation(inputResult.Value)
+            : inputResult.ToResult<TOutput>();
+    }
+}
---
+++ src/EffectiveResult/Extensions/ResultsThenOnFailAsyncExtensions.cs
@@ -0,0 +1,121 @@
+﻿using EffectiveResult.Abstractions;
+
+namespace EffectiveResult.Extensions;
+
+public static partial class ResultsThenOnFailAsyncExtensions
+{
+    /// <summary>
+    /// Call action only if <see cref="input"/> is failed
+    /// </summary>
+    /// <param name="input">Source of conclusion</param>
+    /// <param name="onFailAction">Action for invoke on fail</param>
+    /// <returns>Result from <paramref name="input"/></returns>
+    public static async Task<Result> ThenOnFailAsync(this Result input, Func<Task> onFailAction)
+    {
+        return input.IsFailed
+            ? await Result.TryAsync(onFailAction)
+            : input;
+    }
+
+    /// <summary>
+    /// Call action only if <see cref="input"/> is failed
+    /// </summary>
+    /// <param name="input">Source of conclusion</param>
+    /// <param name="onFailAction">Action for invoke on fail</param>
+    /// <returns>Result from <paramref name="input"/></returns>
+    public static async Task<Result> ThenOnFailAsync(this Result input, Func<IEnumerable<IError>, Task> onFailAction)
+    {
+        return input.IsFailed
+            ? await Result.TryAsync(() => onFailAction(input.Errors))
+            : input;
+    }
+
+    /// <summary>
+    /// Provide chaining method for next operation on failed result.
+    /// </summary>
+    /// <param name="input">Source result</param>
+    /// <param name="continuation">Function for invoke on fail</param>
+    /// <typeparam name="TValue">Type of "<paramref name="continuation"/>" result</typeparam>
+    /// <returns>Result from <see cref="input"/> or result from <paramref name="continuation"/> </returns>
+    public static async Task<Result<TValue>> ThenOnFailAsync<TValue>(
+        this Result<TValue> input,
+        Func<IReadOnlyCollection<IError>, Task<TValue>> continuation)
+    {
+        return input.IsFailed
+            ? await Result.TryAsync(() => continuation(input.Errors))
+            : input;
+    }
+
+    /// <summary>
+    /// Provide chaining method for next operation on failed result.
+    /// </summary>
+    /// <param name="input">Source result</param>
+    /// <param name="continuation">Function for invoke on fail</param>
+    /// <typeparam name="TValue">Type of "<paramref name="continuation"/>" result</typeparam>
+    /// <returns>Result from <see cref="input"/> or result from <paramref name="continuation"/> </returns>
+    public static async Task<Result<TValue>> ThenOnFailAsync<TValue>(
+        this Result<TValue> input,
+        Func<IReadOnlyCollection<IError>, Task<Result<TValue>>> continuation)
+    {
+        return input.IsFailed
+            ? await continuation(input.Errors)
+            : input;
+    }
+
+    /// <summary>
+    /// Provide chaining method for next operation on failed result.
+    /// </summary>
+    /// <param name="input">Source result</param>
+    /// <param name="continuation">Function for invoke on fail</param>
+    /// <typeparam name="TValue">Type of "<paramref name="continuation"/>" result</typeparam>
+    /// <returns>Result from <see cref="input"/> or result from <paramref name="continuation"/> </returns>
+    public static async Task<Result<TValue>> ThenOnFailAsync<TValue>(
+        this Result<TValue> input,
+        Func<Task<TValue>> continuation)
+    {
+        return input.IsFailed
+            ? await Result.TryAsync(continuation)
+            : input;
+    }
+
+    /// <summary>
+    /// Provide chaining method for next operation on failed result.
+    /// </summary>
+    /// <param name="input">Source result</param>
+    /// <param name="continuation">Function for invoke on fail</param>
+    /// <typeparam name="TValue">Type of "<paramref name="continuation"/>" result</typeparam>
+    /// <returns>Result from <see cref="input"/> or result from <paramref name="continuation"/> </returns>
+    public static async Task<Result<TValue>> ThenOnFailAsync<TValue>(
+        this Result<TValue> input,
+        Func<Task<Result<TValue>>> continuation)
+    {
+        return input.IsFailed
+            ? await continuation()
+            : input;
+    }
+
+    /// <summary>
+    /// Call action only if <see cref="input"/> is failed and contains exception with type <see cref="TException"/>
+    /// </summary>
+    /// <param name="input">Source of conclusion</param>
+    /// <param name="onFailAction">Action for invoke on fail</param>
+    /// <typeparam name="TException">Type of searching error</typeparam>
+    /// <returns>Result from <paramref name="input"/></returns>
+    public static async Task<Result> ThenOnFailWithExceptionAsync<TException>(
+        this Result input,
+        Func<IExceptionalError, Task> onFailAction)
+        where TException : Exception
+    {
+        if (input.IsFailed)
+        {
+            var exceptionalError = input.Errors
+                .OfType<IExceptionalError>()
+                .FirstOrDefault(x => x.Exception is TException);
+
+            if (exceptionalError is not null)
+                return await Result.TryAsync(() => onFailAction(exceptionalError));
+        }
+
+        return input;
+    }
+}
